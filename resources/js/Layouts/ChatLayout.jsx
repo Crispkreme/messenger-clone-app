24d3aada5bf35212220497c3c7205b3dcff4a0ef
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-17 16:59:04 -0800

    updated files

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { usePage } from '@inertiajs/react';
+import Echo from 'laravel-echo';
 
 const ChatLayout = ({ children }) => {
 
@@ -9,6 +10,22 @@ const ChatLayout = ({ children }) => {
     
     console.log('conversations: ', conversations);
     console.log('selected conversations: ', selectedConversations);
+
+    useEffect(() => {
+        window.Echo.join('chat-room')
+            .here((users) => {
+                console.log('here', users);
+            })
+            .joining((user) => {
+                console.log('joining', user);
+            })
+            .leaving((user) => {
+                console.log('leaving', user);
+            })
+            .error((error) => {
+                console.log('error', error);
+            });
+    }, []);
 
     return (
         <>
