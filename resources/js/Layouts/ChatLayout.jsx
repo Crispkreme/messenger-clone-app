5aaaf9a2e8a9220736d22218c8e0d04a37ae0a4f
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-05-22 16:47:48 -0800

    updated files
    - adding conversation functionality

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePage } from '@inertiajs/react';
 import Echo from 'laravel-echo';
 
@@ -11,22 +11,84 @@ const ChatLayout = ({ children }) => {
     console.log('conversations: ', conversations);
     console.log('selected conversations: ', selectedConversations);
 
+    const [ setOnlineUsers, isSetOnlineUsers ] = useState({});
+    const isUserOnline = (userId) => onlineUsers[userId];
+
+    // conversations
+    const [ setLocalConversations, isSetLocalConversations ] = useState([]);
+    const [ setSortedConversations, isSetSortedConversations ] = useState([]);
+
     useEffect(() => {
         window.Echo.join('chat-room')
             .here((users) => {
                 console.log('here', users);
+
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+
+                isSetOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
             })
             .joining((user) => {
                 console.log('joining', user);
+
+                isSetOnlineUsers((prevOnlineUsers) => {
+                    const updatedUsers = { ...prevOnlineUsers };
+                    updatedUsers[user.id] = user;
+                    return updatedUsers;
+                });
             })
             .leaving((user) => {
                 console.log('leaving', user);
+
+                isSetOnlineUsers((prevOnlineUsers) => {
+                    const updatedUsers = { ...prevOnlineUsers };
+                    delete updatedUsers[user.id];
+                    return updatedUsers;
+                });
             })
             .error((error) => {
                 console.log('error', error);
             });
+
+        return () => {
+            window.Echo.leave('online');
+        }
     }, []);
 
+    // check if conversation is change
+    useEffect(() => {
+        isSetLocalConversations(conversations);
+    }, [conversations]);
+
+    useEffect(() => {
+        isSetSortedConversations(
+            setLocalConversations.sort((a, b) => {
+                if(a.blocked_at && b.blocked_at) {
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                }else if(a.blocked_at) {
+                    return 1;
+                }else if(b.blocked_at){
+                    return -1;
+                }
+
+                if(a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.localeCompare(
+                        a.last_message_date
+                    )
+                } else if(a.last_message_date) {
+                    return -1;
+                } else if(b.last_message_date) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }),
+        );
+    }, [setLocalConversations]);
+    
     return (
         <>
             ChatLayout
--- routes/channels.php
+++ routes/channels.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Broadcast;
 
-// Broadcast::channel('online', function ($user) {
-//     return $user;
-// });
-
 Broadcast::channel('chat-room', function ($user) {
-    return auth()->user();
+    return auth()->user() ? new UserResource($user) : null;
 });
